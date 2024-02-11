8a958774dbf71c48dc96f06ceff953f1ea910e5d
vitodursopettersson <vitodurso98@gmail.com>
2024-02-11 19:33:09 +0100

    feat: validation edit project request

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -6,15 +6,27 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Titolo progetto</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $project->title }}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                value="{{ old('title', $project->title) }}">
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea class="form-control" id="description" rows="3" name="description">{{ $project->description }}</textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" rows="3"
+                name="description">{{ old('description', $project->description) }}</textarea>
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="year" class="form-label">Anno</label>
-            <input type="text" class="form-control" id="year" name="year" value="{{ $project->year }}">
+            <input type="text" class="form-control @error('year') is-invalid @enderror" id="year" name="year"
+                value="{{ old('year', $project->year) }}">
+            @error('year')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <input type="submit" class="btn btn-primary" value="Salva">
     </form>
