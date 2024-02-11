5a0d95b1a9b18b9db31775f37c42be087132b0d9
vitodursopettersson <vitodurso98@gmail.com>
2024-02-11 18:19:21 +0100

    feat: show project crud

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1><a href="{{ route('admin.projects.index') }}">Torna indietro</a></h1>
+    <h2>Progetto:</h2>
+    <h3>{{ $project->title }}</h3>
+    <p>{{ $project->description }}</p>
+    <h6>{{ $project->year }}</h6>
+@endsection
